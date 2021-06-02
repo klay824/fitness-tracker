520b1249ce92ffef59ca6e08dac023c51d1e2126
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-01 19:30:53 -0500

    added routes to server file

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnessdb', {
     useFindAndModify: false
 });
 
+app.use(require('./routes/api-routes.js'));
+app.use(require('./routes/html-routes.js'));
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 })
